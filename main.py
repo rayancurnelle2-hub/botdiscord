53cc0bcd73a6de2fab3f79d05a7374309348b7e4
rayancurnelle2-hub <rayancurnelle2@gmail.com>
2026-04-20 15:40:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ TOKEN = os.getenv("TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 voice_client = None
 last_channel = None
